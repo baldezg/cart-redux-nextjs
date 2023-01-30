d35cabb37354c060b5e8db5430efccc4ea25dab0
baldezg <gabrielbaldez34@gmail.com>
2023-01-29 23:21:27 -0300

    Incluindo o provider no ambiente de testes

--- src/__tests__/index.test.tsx
+++ src/__tests__/index.test.tsx
@@ -1,19 +1,30 @@
 import { render, screen } from '@testing-library/react';
 import Home from '../pages/index';
 import '@testing-library/jest-dom';
+import { Provider } from 'react-redux';
+import store from '../redux/store';
 
 
 describe('Home page', () => {
     it('O header foi renderizado', () => {
         const data = { products: [] };
-        render(<Home data={data}/>);
+        render(
+        <Provider store={store}>
+        <Home data={data}/>
+        </Provider>
+        );
         const header = screen.getByTestId('header');
         expect(header).toBeInTheDocument();
     })
     
     it('Skeleton renderiza enquanto os dados não são carregados', () => {
         const data = { products: [] };
-        const {container} = render(<Home data={data}/>);
+        const {container} = 
+        render(
+            <Provider store={store}>
+                <Home data={data}/>
+            </Provider>
+        );
         const skeleton = container.querySelector('.react-loading-skeleton');
         expect(skeleton).toBeInTheDocument();
     })
@@ -21,21 +32,24 @@ describe('Home page', () => {
     it('Os produtos são mostrados na tela', () => {
         const data = { products: [
             {
-                id: 1,
+                id: '1',
                 name: 'Produto 1',
                 description: 'Descrição do produto 1',
                 photo: 'https://mks-challenge-api-frontend.herokuapp.com/images/1.jpg',
                 price: 'R$ 100,00'
             },
             {
-                id: 2,
+                id: '2',
                 name: 'Produto 2',
                 description: 'Descrição do produto 2',
                 photo: 'https://mks-challenge-api-frontend.herokuapp.com/images/2.jpg',
                 price: 'R$ 200,00'
             },
         ]};
-        render(<Home data={data}/>);
+        render(
+        <Provider store={store}>
+        <Home data={data}/>
+        </Provider>);
         expect(screen.getByText('Produto 1')).toBeInTheDocument();
         expect(screen.getByText('Produto 2')).toBeInTheDocument();
     })
